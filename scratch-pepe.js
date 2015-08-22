f4c4ae3b2c53bd2fdc780e552caaa74f48051760
Pepe Bawagan <syk0saje@gmail.com>
2015-08-22 17:08:52 +0800

    precollects agencies

--- scratch-pepe.js
+++ scratch-pepe.js
@@ -1,8 +1,28 @@
+var agencies = [];
+
 Papa.parse("adiaug2015.csv", {
 	download: true,
+	header: true,
 	complete: function (results, file){
-		console.log(results);
+
+		var agencyNames = [];
+		var agencyAbbrs = [];
+		
+		results.data.forEach(function (row){
+			var agencyName = row.agency_name;
+			if (!_.includes(agencyNames, agencyName)){
+				var agencyAbbr = row.agency_abbrv;
+				agencyNames.push(agencyName);
+				agencyAbbrs.push(agencyAbbr);
+			}
+		});
+
+		for(var i in agencyNames){
+			agencies.push({
+				name: agencyNames[i],
+				abbr: agencyAbbrs[i]
+			});
+		}
+
 	}
 });
-
-console.log("May kuko sa titi ko");
