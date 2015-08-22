66e4d5d6a89c34e4c869e97501d4c6075a74c92b
Levi Tan Ong <levi@byimplication.com>
2015-08-22 16:09:00 +0800

    makes separate scratch

---
+++ scratch-levi.js
@@ -0,0 +1,6 @@
+Papa.parse("adiaug2015.csv", {
+  download: true,
+  complete: function (results, file){
+    console.log(results);
+  }
+});
--- scratch-pepe.js
+++ scratch-pepe.js
@@ -1,8 +1,8 @@
-console.log("Hello world!");
-
 Papa.parse("adiaug2015.csv", {
 	download: true,
 	complete: function (results, file){
 		console.log(results);
 	}
 });
+
+console.log("May kuko sa titi ko");
