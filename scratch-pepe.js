45b9b77cc80c526e80bc46a51c2d65842feca070
Pepe Bawagan <syk0saje@gmail.com>
2015-08-22 17:46:06 +0800

    cleans up agency preprocessing, defines classes (Agency, Entry, etc.)

--- scratch-pepe.js
+++ scratch-pepe.js
@@ -1,28 +1,52 @@
+var entries = [];
 var agencies = [];
+
+function Agency(name, abbr){
+	this.name = name;
+	this.abbr = abbr;
+}
+
+function Entry(row){
+
+	this.title = row.title;
+	this.description = row.description;
+	this.onlinePub = row.online_publication == "Yes";
+	this.disclosive = row.disclosive;
+	this.maintainer = row.data_maintainer;
+	this.url = row.location_or_url;
+	this.source = row.original_data_owner;
+
+	// TODO - this.agency = findAgency(row.agency_name)
+
+	// TODO - file format
+	this.fileFormat = row.file_format;
+	
+	// TODO - standardize date released
+	this.dateReleased = row.date_released;
+	
+	// TODO - preprocess frequency of update
+	this.updateFreq = row.frequency_of_update
+
+}
+
+function EntrySet(entries){
+
+}
 
 Papa.parse("adiaug2015.csv", {
 	download: true,
 	header: true,
 	complete: function (results, file){
 
-		var agencyNames = [];
-		var agencyAbbrs = [];
-		
-		results.data.forEach(function (row){
-			var agencyName = row.agency_name;
-			if (!_.includes(agencyNames, agencyName)){
-				var agencyAbbr = row.agency_abbrv;
-				agencyNames.push(agencyName);
-				agencyAbbrs.push(agencyAbbr);
-			}
-		});
+		agencies = _.chain(results.data).unique(false, function (row){
+			return row.agency_name;
+		}).map(function (row){
+			return new Agency(row.agency_name, row.agency_abbrv);
+		}).value();
 
-		for(var i in agencyNames){
-			agencies.push({
-				name: agencyNames[i],
-				abbr: agencyAbbrs[i]
-			});
-		}
+		entries = _.chain(results.data).map(function (row){
+			return new Entry(row);
+		});
 
 	}
 });
