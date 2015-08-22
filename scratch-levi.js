9b3e9d11d01e22c3c04689de75cb49816c6c18df
Levi Tan Ong <levi@byimplication.com>
2015-08-22 17:11:08 +0800

    deals with year disclosives

--- scratch-levi.js
+++ scratch-levi.js
@@ -2,6 +2,21 @@ Papa.parse("adiaug2015.csv", {
   download: true,
   header: true,
   complete: function (results, file){
-    console.log(results.data, _);
+    console.log(results.data, _, Set);
+    const disclosiveValues = new Set(
+      results.data.map(function(dataset){
+        return dataset.disclosive
+      })
+    )
+
+    const assessDisclosiveValue = function(disclosive) {
+      if (disclosive.match(/\d{4}-\d{4}/)){
+        return disclosive.split("-").map(function(year){
+          return _.parseInt(year);
+        });
+      }
+    }
+
+    disclosiveValues.forEach(function(d){console.log(assessDisclosiveValue(d))});
   }
 });
